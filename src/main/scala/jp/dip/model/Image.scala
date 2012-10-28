013601a2a48dd997e586c81ee6b10a89f186d68e
MaruyaYoshihisa <m0029.swim@gmail.com>
2012-10-28 13:36:05 +0900

    ビデオファイルへの対応 v1

--- src/main/scala/jp/dip/model/Image.scala
+++ src/main/scala/jp/dip/model/Image.scala
@@ -1,18 +1,78 @@
 package jp.dip.model
 
 import net.liftweb.mapper._
+import net.liftweb.http.FileParamHolder
+import net.liftweb.util.Props
+import java.io.InputStream
+import scala.sys.process.Process
+import java.io.FileOutputStream
+import org.apache.commons._
 
-class Image extends LongKeyedMapper[Image] with IdPK {     
-  def getSingleton = Image                                 
+class Image extends LongKeyedMapper[Image] with IdPK {
+  def getSingleton = Image
 
-  object lat extends MappedString(this, 100)
-  object lon extends MappedString(this, 100)
-  object wheather extends MappedString(this, 100)  
-  object file_path extends MappedString(this,100)
-  object created_datetime extends MappedDateTime(this)
+  object lat extends MappedString(this, 100) //軽度
+  object lon extends MappedString(this, 100) //緯度
+  object wheather extends MappedString(this, 100) //天気 
+  object mimeType extends MappedString(this, 100) //ファイル形式
+  object video_file_path extends MappedString(this, 100) //ビデオファイルパス
+  object thumbnail_file_path extends MappedString(this, 100) //サムネイルパス
+  object created_datetime extends MappedDateTime(this) //作成時間
+  
+  /**
+   * ファイルを保存(パスはprops参照)
+   * (id,f) : file_path
+   */
+  def saveFile(f: FileParamHolder): (String,String) = {
+    //保存用ディレクトリ
+    val save_dir = Props.get("save.dirpath") openOr
+      "/Users/tmp/"
+
+    //ローカルファイル書き込み用
+    /**
+     * 動画を保存。サムレイルを作成
+     */
+    def write(extention: String, is: InputStream): (String,String) = {
+      //一時ファイルを作成
+      val tmp_file_path = save_dir + id + "." + extention
+      val os = new FileOutputStream(tmp_file_path)
+      try{
+    	  org.apache.commons.io.IOUtils.copy(is, os)
+      }catch {
+        case e =>  throw new Exception(e.getMessage())
+      } finally {
+        os.close
+      }
+      
+      //ビデオとサムレイルを作成
+      val video_file_path = save_dir + id + ".flv"
+      val thumbnail_file_path = save_dir + id + ".jpg"
+      println("####" + video_file_path + "####")
+      extention match {
+      	case "mp4" => Process("mv " + tmp_file_path + " "+ video_file_path).run
+      	case "mov" => Process("ffmpeg -i " + tmp_file_path +" " + video_file_path + ";rm " + tmp_file_path).run
+      	case _ => null
+      }
+      Process("ffmpeg -i " + video_file_path +" -ss 1 -vframes 1 -f image2 " + thumbnail_file_path)
+      (video_file_path,thumbnail_file_path)
+    }
+
+    //ファイル形式によって、コマンドを分ける。
+    val extention = f.mimeType match {
+      case "video/mp4" => "mp4"
+      case "video/MOV" => "mov"
+      case _ => throw new Exception("ファイル形式をサポートしていません。")
+    }
+    try{
+      write(extention,f.fileStream)
+    }
+    catch{
+      case e => throw new Exception(e.getMessage())
+    }
+  }
 }
 
 object Image extends Image with LongKeyedMetaMapper[Image] {
-  override def dbTableName = "Images"
+  override def dbTableName = "Images" 
 }
 
--- src/main/scala/jp/dip/snippet/Postimage.scala
+++ src/main/scala/jp/dip/snippet/Postimage.scala
@@ -6,10 +6,12 @@ import _root_.net.liftweb.common._
 import _root_.java.util.Date
 import jp.dip.lib._
 import Helpers._
-import net.liftweb.http.FileParamHolder
-import net.liftweb.http.RequestVar
+import net.liftweb.http.{ FileParamHolder, RequestVar, S }
 import net.liftweb.util.Helpers._
 import net.liftweb.http.SHtml
+import jp.dip.model.Image
+import java.io.InputStream
+import java.io.FileOutputStream
 
 class Postimage {
   //フォーム用の宣言
@@ -17,18 +19,35 @@ class Postimage {
   object lat extends RequestVar(Empty)
   object lon extends RequestVar(Empty)
 
-  def upload(xhtml: NodeSeq): NodeSeq = {
-    def add() {
-      val a = Full(img.is)
-      aa
-      img.is match {
-        case Full(FileParamHolder(_, mine, fname, file)) => {
-          println(mine)
+  private def log(st: String) {
+    println("##### " + st + "####")
+  }
+
+  /**
+   * ImageTableに保存
+   */
+  private def add() {
+     val f = img.is.openOr(null)
+     f match {
+      case FileParamHolder(_, mine, fname, file) => {
+        val image_model = Image.create
+        image_model.save() //idを取得するために、一度保存
+        try{
+        	val file_path = image_model.saveFile(f)
+        	image_model.mimeType(mine).save()
         }
-
+        catch{
+          case e => S.error(e.getMessage())
+        }
       }
-
+      case _ => S.error("ファイルを選択してください");
     }
+  }
+
+  /**
+   * ファイルアップロード用フォーム
+   */
+  def upload(xhtml: NodeSeq): NodeSeq = {
 
     bind("upload", xhtml,
       "file" -> SHtml.fileUpload(fh => img(Full(fh))),
